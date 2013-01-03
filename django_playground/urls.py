7e6208f52447a311b732c9052b4e549d5efe617d
Jim Munro <jim.munro@sendgrid.com>
2013-01-03 16:13:23 -0700

    got the basics working with model forms and class based generic views

--- django_playground/urls.py
+++ django_playground/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django_playground.views import PlaygroundFormView
+from django_playground.views import PlaygroundCreateView, PlaygroundUpdateView, PlaygroundListView, PlaygroundDetailView
 from django.views.generic import TemplateView
 
 from django.contrib import admin
@@ -8,7 +8,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
-    url(r'^playground-form/$', PlaygroundFormView.as_view(), name='playground_form'),
-    url(r'^form-success/$', TemplateView.as_view(template_name="success.html"), name="success"), 
+    url(r'^playground/$', PlaygroundListView.as_view(), name="playground_list"),
+    url(r'^playground/(?P<pk>\d+)/$', PlaygroundDetailView.as_view(), name="playground_detail"),
+    url(r'^playground/create/$', PlaygroundCreateView.as_view(), name='playground_form'),
+    url(r'^playground/(?P<id>\d+)/update/$', PlaygroundUpdateView.as_view(), name="playground_update"),
+    url(r'^form-success/$', TemplateView.as_view(template_name="django_playground/success.html"), name="success"), 
     url(r'^admin/', include(admin.site.urls)),
 )
--- django_playground/views.py
+++ django_playground/views.py
@@ -1,7 +1,33 @@
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.http import Http404, HttpResponse
 from django_playground.forms import PlaygroundForm
+from django_playground.models import Playground
+from django.core.urlresolvers import reverse
 
-class PlaygroundFormView(FormView):
-	template_name = 'playground_form.html'
+class PlaygroundCreateView(CreateView):
+	template_name = 'django_playground/playground_form.html'
 	form_class = PlaygroundForm
-	success_url = '/'
+	success_url = "/playground/"
+
+class PlaygroundUpdateView(UpdateView):
+	template_name = 'django_playground/playground_form.html'
+	form_class = PlaygroundForm
+	success_url = "/playground/"
+
+	def get_object(self, queryset=None):
+		try:
+			return Playground.objects.get(pk=self.kwargs['id'])
+		except Playground.DoesNotExist:
+			raise Http404
+
+class PlaygroundListView(ListView):
+	model = Playground
+	template_name = 'playground_list.html'
+	context_object_name = 'playground_list'
+
+class PlaygroundDetailView(DetailView):
+	template_name = 'playground_detail.html'
+	context_object_name = 'playground'
+	model = Playground
